6587c04ef98439d65c061d95a80c76b65c0454b0
Travis-Taylor <stygian.stevedore@gmail.com>
2025-10-14 23:54:15 -0600

    Support Fluent-bit Forwarding (#2)
    * Warn on connection failure
    
    * Format
    
    * WIP debug mess
    
    * Actually fucking works
    
    * Cleanup
    
    * Add config state to Sender server
    
    * Add extra_fields for sender payload
    
    * Edit tag with log level, if passed
    
    * Explicitly skip deprecated :warn level

--- lib/logger_fluentd_backend/logger.ex
+++ lib/logger_fluentd_backend/logger.ex
@@ -48,6 +48,8 @@ defmodule LoggerFluentdBackend.Logger do
   ## Helpers
 
   defp meet_level?(_lvl, nil), do: true
+  # Somewhere deep in the deps, something is using deprecated :warn level. Don't let them
+  defp meet_level?(:warn, _), do: false
   defp meet_level?(lvl, min), do: Logger.compare_levels(lvl, min) != :lt
 
   defp configure(options) do
@@ -61,31 +63,43 @@ defmodule LoggerFluentdBackend.Logger do
     tag = Keyword.get(config, :tag) || ""
     level = Keyword.get(config, :level)
     # metadata = Keyword.get(config, :metadata, [])
+    # Extra fields to emit with the log payload
+    extra_fields = Keyword.get(config, :extra_fields, %{})
 
-    %{level: level, host: host, port: port, tag: tag, serializer: serializer}
+    # Configure Sender state as well
+    Sender.configure(host: host, port: port, serializer: serializer, extra_fields: extra_fields)
+
+    %{level: level, tag: tag}
   end
 
   defp configure_merge(env, options) do
     Keyword.merge(env, options, fn _, _v1, v2 -> v2 end)
   end
 
-  defp log_event(level, msg, _ts, md, %{tag: tag} = state) do
+  defp log_event(level, msg, _ts, md, %{tag: tag}) do
     f =
       case md[:function] do
         {f, a} -> "#{f}/#{a}"
         _ -> ""
       end
 
+    # TODO(ttaylor) Re-add module? Check w/ maintainer
+    filename =
+      md
+      |> Keyword.get(:file, "")
+      |> Path.rootname()
+      |> Path.basename()
+
     data = %{
       pid: inspect(md[:pid]),
-      module: inspect(md[:module]),
+      filename: filename,
       function: f,
-      line: inspect(md[:module]),
+      line: inspect(md[:line]),
       level: to_string(level),
       message: to_string(msg),
       payload: md[:payload]
     }
 
-    Sender.send(tag, data, state.host, state.port, state.serializer)
+    Sender.send(tag, data)
   end
 end
--- lib/logger_fluentd_backend/sender.ex
+++ lib/logger_fluentd_backend/sender.ex
@@ -4,11 +4,18 @@ defmodule LoggerFluentdBackend.Sender do
   alias Socket.Stream
   alias Socket.TCP
 
+  require Logger
+
   defmodule State do
-    defstruct socket: nil
+    defstruct socket: nil,
+              host: nil,
+              port: nil,
+              serializer: nil,
+              extra_fields: %{},
+              connection_failure_warned: false
   end
 
-  def start_link([])do
+  def start_link([]) do
     GenServer.start_link(__MODULE__, :ok, name: __MODULE__)
   end
 
@@ -16,12 +23,18 @@ defmodule LoggerFluentdBackend.Sender do
     {:ok, %State{socket: nil}}
   end
 
-  def send(tag, data, host, port, serializer) do
+  def configure(options) do
+    :ok = GenServer.cast(__MODULE__, {:configure, options})
+  end
+
+  def send(tag, data) do
+    :ok = GenServer.cast(__MODULE__, {:send, tag, data, []})
+  end
+
+  def send(tag, data, host, port, serializer \\ :msgpack) do
     options = [host: host, port: port, serializer: serializer]
     :ok = GenServer.cast(__MODULE__, {:send, tag, data, options})
   end
-
-  def send(tag, data, host, port), do: send(tag, data, host, port, :msgpack)
 
   def stop() do
     GenServer.call(__MODULE__, {:stop, []})
@@ -31,7 +44,9 @@ defmodule LoggerFluentdBackend.Sender do
     {:reply, :ok, %State{socket: nil}}
   end
 
-  def terminate(_reason, %State{socket: socket}) when not is_nil(socket) do
+  def terminate(_reason, %State{socket: nil}), do: :ok
+
+  def terminate(_reason, %State{socket: socket}) do
     Stream.close(socket)
   end
 
@@ -39,31 +54,123 @@ defmodule LoggerFluentdBackend.Sender do
     GenServer.start_link(__MODULE__, [], name: __MODULE__)
   end
 
+  def handle_cast({:configure, options}, state) do
+    {:noreply, construct_state(state.socket, options, state.connection_failure_warned)}
+  end
+
   def handle_cast({_, _, _, options} = msg, %State{socket: nil} = state) do
-    socket = connect(options)
-    handle_cast(msg, %State{state | socket: socket})
+    options = resolve_options(options, state)
+
+    case connect(options, state) do
+      %State{socket: nil} = state ->
+        {:noreply, state}
+
+      state ->
+        handle_cast(msg, state)
+    end
   end
 
   def handle_cast({:send, tag, data, options}, %State{socket: socket} = state) do
-    packet = serializer(options[:serializer]).([tag, now(), data])
+    options = resolve_options(options, state)
+    payload = construct_payload(tag, data, options[:extra_fields])
+    packet = serializer(options[:serializer]).(payload)
     Stream.send!(socket, packet)
     {:noreply, state}
   end
 
-  defp connect(options) do
-    TCP.connect!(
-      options[:host] || "localhost",
-      options[:port] || 24224,
-      packet: 0
-    )
+  # Try to connect a socket, returning the state with the resulting socket if successful
+  @spec connect(keyword(), %State{}) :: %State{}
+  defp connect(options, %{connection_failure_warned: warned} = state) do
+    options = resolve_options(options, state)
+    host = options[:host]
+    port = options[:port]
+
+    case TCP.connect(host, port, packet: 0) do
+      {:ok, socket} ->
+        construct_state(socket, options, false)
+
+      {:error, err} ->
+        if not warned do
+          Logger.error(
+            "Unable to connect TCP socket at #{host}:#{port} for fluent logger: #{err} "
+          )
+        end
+
+        construct_state(nil, options, true)
+    end
   end
+
+  # Use explicitly passed options; or configured values in state if not specified; or
+  # fall back to default values
+  @spec resolve_options(keyword(), %State{}) :: keyword()
+  defp resolve_options(options, state) do
+    host = options[:host] || Map.get(state, :host, "localhost")
+    port = options[:port] || Map.get(state, :port, 24224)
+    serializer = options[:serializer] || Map.get(state, :serializer, :msgpack)
+    extra_fields = options[:extra_fields] || state.extra_fields
+
+    [host: host, port: port, serializer: serializer, extra_fields: extra_fields]
+  end
+
+  defp construct_payload(tag, data, extra_fields) do
+    tag = construct_tag(tag, data)
+    # Fluent-bit expects an EXT type for Forward input timestamp (10-bytes, w/ 4B epoch
+    # seconds and 4B ns)
+    now = now()
+
+    {now_s, now_ms} =
+      now
+      |> Decimal.from_float()
+      |> Decimal.div_rem(1)
+
+    now_s = Decimal.to_integer(now_s)
+
+    now_ns =
+      now_ms
+      |> Decimal.mult(1_000_000)
+      |> Decimal.to_integer()
+
+    # Insert D7 (fixext 8), 00 (integer type), and time (2-part integer).
+    # spec for ref: https://github.com/msgpack/msgpack/blob/master/spec.md#formats
+    # NOTE: must be big-endian (elixir bitstring default)
+    time_bitstring =
+      <<0xD7, 0x00>> <> <<now_s::unsigned-size(32)>> <> <<now_ns::unsigned-size(32)>>
+
+    time_binary = Msgpax.unpack!(time_bitstring)
+
+    # Merge in configured extra fields with data payload
+    data = Map.merge(extra_fields, data)
+    [tag, time_binary, data]
+  end
+
+  def construct_tag(tag, %{level: log_level}) when is_binary(log_level) do
+    case tag do
+      "" -> log_level
+      tag when is_binary(tag) -> "#{tag}.#{log_level}"
+    end
+  end
+
+  def construct_tag(tag, _data), do: tag
 
   defp serializer(:msgpack), do: &Msgpax.pack!/1
   defp serializer(:json), do: &Jason.encode!/1
   defp serializer(f) when is_function(f, 1), do: f
 
+  # Helper to construct the server state with the expected fields
+  @spec construct_state(TCP.t() | nil, keyword(), boolean()) :: %State{}
+  defp construct_state(socket, options, failure_warning) do
+    %State{
+      socket: socket,
+      connection_failure_warned: failure_warning,
+      host: options[:host],
+      port: options[:port],
+      serializer: options[:serializer],
+      extra_fields: Keyword.get(options, :extra_fields, %{})
+    }
+  end
+
   defp now() do
-    {msec, sec, _} = :os.timestamp()
-    msec * 1_000_000 + sec
+    {megasec, sec, usec} = :os.timestamp()
+    megasec * 1_000_000 + sec + usec / 1_000_000
   end
 end
